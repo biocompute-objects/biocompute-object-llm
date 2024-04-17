50a7e109418b09230a3989bcc71c936b2121eaf0
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-04-17 11:54:56 -0400

    hardcode usability domain for testing

--- rag/main.py
+++ rag/main.py
@@ -4,18 +4,15 @@ from bcorag import misc_functions as misc_fns
 def main():
 
     logger = misc_fns.setup_root_logger("./logs/bcorag.log")
-    logger.info('################################## RUN Start ##################################')
+    logger.info('################################## RUN START ##################################')
     
     # get the user choices
     user_picks = pipeline.initalize_step()
     if user_picks is None:
         misc_fns.graceful_exit()
-    for option, selection in user_picks.items():
-        logger.info("User selections:")
-        logger.info(f"\t{option}: {selection}")
 
     # handle domain generation
-    bco_rag = pipeline.retrieve_bco_rag(user_picks)
+    bco_rag = pipeline.retrieve_bco_rag(user_picks) # type: ignore
     print(bco_rag.perform_query("usability"))
 
 if __name__ == "__main__":
