ad4400f15960a26c76f6d6082b2939be56821229
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-04-17 15:40:45 -0400

    add None response handling

--- rag/main.py
+++ rag/main.py
@@ -13,7 +13,9 @@ def main():
 
     # handle domain generation
     bco_rag = pipeline.retrieve_bco_rag(user_picks) # type: ignore
-    print(bco_rag.perform_query("usability"))
+    response = bco_rag.choose_domain(automatic_query=True)
+    if response is None:
+        misc_fns.graceful_exit()
 
 if __name__ == "__main__":
     main()
